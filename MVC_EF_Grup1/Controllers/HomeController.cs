fafff274dc4a236341056663c953bba09ac72950
hikmetcnli <hikmetcnli@hotmail.com>
2026-03-12 13:02:21 +0300

    Add project files.

---
+++ MVC_EF_Grup1/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using MVC_EF_Grup1.Models;
+
+namespace MVC_EF_Grup1.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            Context c = new Context();  
+
+            var ProductList = c.Products_.ToList(); 
+
+            return View(ProductList);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ MVC_EF_Grup1/Models/Context.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MVC_EF_Grup1.Models
+{
+    public class Context: DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=GDKTP-NB-000;Database=DB_Grup1;User ID=hikmetcanli;Password=1234;Trusted_Connection=False;TrustServerCertificate=True");
+        }
+
+        public DbSet<Product> Products_ { get; set; }
+
+    }
+}
