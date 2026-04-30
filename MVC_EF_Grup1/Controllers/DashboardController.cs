8fd597d2f499100419c575534234c766ed5372b3
hikmetcnli <hikmetcnli@hotmail.com>
2026-04-30 14:47:52 +0300

    SSS

--- MVC_EF_Grup1/Controllers/DashboardController.cs
+++ MVC_EF_Grup1/Controllers/DashboardController.cs
@@ -15,10 +15,10 @@ namespace MVC_EF_Grup1.Controllers
             ViewBag.SonBirAydakiSatinAlma = c.SatinAlmas_.Where(x => x.AlimTarihi >= DateTime.Now.AddDays(-30)).Sum(x => x.AlimFiyati);
 
 
-            //Trend Raporları
+            //Trend Raporları 
             //ViewBag.CountriesText = new List<string> { "Italy", "France", "Spain", "USA", "Argentina" };
             //ViewBag.CountriesValue = new List<int> { 55, 49, 44, 24, 15 };
-
+            //Aylık SatınAlma
             var aylikAlimFiyatlari = c.SatinAlmas_.
                         GroupBy(x => new { x.AlimTarihi.Year, x.AlimTarihi.Month }).
                         Select(y => new
@@ -39,8 +39,54 @@ namespace MVC_EF_Grup1.Controllers
 
             ViewBag.SatinAlmaText = SatinAlmaText;
             ViewBag.SatinAlmaValue = SatinAlmaValue;
+            /*******************************************/
 
-            //
+            //Araç Markasına Göre Satın Alma Dağılımı
+            var AracMarkasinaGoreAlimFiyati = c.SatinAlmas_.
+                Include(c=>c.Arac).
+                GroupBy(x=> new {x.Arac.Marka}).
+                Select( y=> new
+                {
+                    AracMarka = y.Key.Marka,
+                    ToplamSatinAlmaFiyat = y.Sum(s => s.AlimFiyati)
+                 }).OrderByDescending(y => y.AracMarka).ToList();
+
+            List<string> AracMarkaText = new List<string>();
+            List<int> AracMarkaValue = new List<int>();
+
+            foreach (var dr in AracMarkasinaGoreAlimFiyati)
+            {
+                AracMarkaText.Add(dr.AracMarka);
+                AracMarkaValue.Add(Convert.ToInt32(dr.ToplamSatinAlmaFiyat));
+            }
+
+            ViewBag.AracMarkaText = AracMarkaText;
+            ViewBag.AracMarkaValue = AracMarkaValue;
+            /*******************************************/
+
+            //Müşteriye Göre Satın Alma Dağılımı
+            var MusteriGoreAlimFiyati = c.SatinAlmas_.
+                Include(c => c.Musteri).
+                GroupBy(x => new { x.Musteri.MusteriAdi, x.Musteri.MusteriSoyadi }).
+                Select(y => new
+                {
+                    Musteri = y.Key.MusteriAdi + " " + y.Key.MusteriSoyadi,
+                    ToplamSatinAlmaFiyat = y.Sum(s => s.AlimFiyati)
+                }).OrderByDescending(y => y.Musteri).ToList();
+
+            List<string> MusteriText = new List<string>();
+            List<int> MusteriValue = new List<int>();
+
+            foreach (var dr in MusteriGoreAlimFiyati)
+            {
+                MusteriText.Add(dr.Musteri);
+                MusteriValue.Add(Convert.ToInt32(dr.ToplamSatinAlmaFiyat));
+            }
+
+            ViewBag.MusteriText = MusteriText;
+            ViewBag.MusteriValue = MusteriValue;
+            /*******************************************/
+
 
 
             var SonSatinAlmaKayitlari = c.SatinAlmas_.
